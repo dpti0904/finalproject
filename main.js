1ba0d997619383bb8d0b0a6b5ad5d115f730c78c
DeeptiR <133840526+dpti0904@users.noreply.github.com>
2023-12-08 17:00:33 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-//  track number of skills dragged into each box
+// Track the number of skills dragged into each box
 let skillsCount = 0;
 let skills2Count = 0;
 let skills3Count = 0;
@@ -13,6 +13,13 @@ const badSkills2 = ["Data Entry Intern at FedEx", "Office Assistant at Mary's Co
 const goodSkills3 = ["Secure Blogging Platform with Multi-Factor Authentication", "AI-Powered Personal Portfolio Optimization", "Blockchain-Based Task Management Application"];
 const badSkills3 = ["Portfolio Website", "To Do List App", "Social Media Metrics Dashboard"];
 
+// Point system
+const pointSystem = {
+    goodSkill: 1, // Points for each good skill
+    badSkill: -1, // Points for each bad skill
+};
+
+
 function allowDrop(event) {
     event.preventDefault();
 }
@@ -26,13 +33,13 @@ function drop(event, targetId) {
     var data = event.dataTransfer.getData("text");
 
     // Check the targetId to determine the destination box
-    if (targetId === "otherSkillsList" && skillsCount < 5) {
+    if (targetId === "otherSkillsList" && skillsCount < 7) {
         addToBox(data, targetId);
         skillsCount++;
-    } else if (targetId === "workExperienceList" && skills2Count < 5) {
+    } else if (targetId === "workExperienceList" && skills2Count < 6) {
         addToBox(data, targetId);
         skills2Count++;
-    } else if (targetId === "projectsList" && skills3Count < 3) {
+    } else if (targetId === "projectsList" && skills3Count < 4) {
         addToBox(data, targetId);
         skills3Count++;
     }
@@ -42,7 +49,7 @@ function addToBox(data, targetId) {
     var newElement = document.createElement("li");
     newElement.innerText = data;
 
-    // Add remove button to the new element
+    // Add a "Remove" button to the new element
     var removeButton = document.createElement("button");
     removeButton.textContent = "Remove";
     removeButton.className = "remove-btn";
@@ -63,34 +70,59 @@ function addToBox(data, targetId) {
     document.getElementById(targetId).appendChild(newElement);
 }
 
+
+
+
+function calculatePoints(goodSkills, badSkills, userSkills) {
+    let points = 0;
+
+    userSkills.forEach(skill => {
+        const lowercaseSkill = skill.toLowerCase();  // Convert skill to lowercase for case-insensitive comparison
+        if (goodSkills.map(s => s.toLowerCase()).includes(lowercaseSkill)) {
+            points += pointSystem.goodSkill;
+        } else if (badSkills.map(s => s.toLowerCase()).includes(lowercaseSkill)) {
+            points += pointSystem.badSkill;
+        } else {
+            console.log(`Unknown skill: ${skill}`);
+        }
+    });
+
+    return points;
+}
+
+
+// ... (existing code)
+
+function redirectToNextPage(hasInterviewOffer) {
+    // Store the result in sessionStorage to retrieve it on nextpage.html
+    sessionStorage.setItem("interviewResult", hasInterviewOffer ? "Congratulations! You got an interview offer!" : "Sorry, you did not qualify for an interview offer.");
+    window.location.href = "nextpage.html";
+}
+
 function checkInterviewOffer() {
     const workExperienceSkills = getSkillsFromList("workExperienceList");
     const otherSkills = getSkillsFromList("otherSkillsList");
     const projectsSkills = getSkillsFromList("projectsList");
 
+    // Calculate points for each category
+    const points1 = calculatePoints(goodSkills1, badSkills1, otherSkills);
+    const points2 = calculatePoints(goodSkills2, badSkills2, workExperienceSkills);
+    const points3 = calculatePoints(goodSkills3, badSkills3, projectsSkills);
+
     // Check if the user meets the criteria for an interview offer
-    if (
-        hasEnoughSkills(goodSkills2, workExperienceSkills, 3) &&
-        hasEnoughSkills(badSkills2, workExperienceSkills, 2) &&
-        hasEnoughSkills(goodSkills1, otherSkills, 3) &&
-        hasEnoughSkills(badSkills1, otherSkills, 2) &&
-        hasEnoughSkills(goodSkills3, projectsSkills, 2) &&
-        hasEnoughSkills(badSkills3, projectsSkills, 1)
-    ) {
-        alert("Congratulations! You got an interview offer!");
-    } else {
-        alert("Sorry, you did not qualify for an interview offer.");
-    }
+    const hasInterviewOffer = points1 >= 3 && points2 >= 3 && points3 >= 2;
+
+    // Redirect to nextpage.html without displaying an alert
+    redirectToNextPage(hasInterviewOffer);
 }
+
+
 
 function getSkillsFromList(listId) {
     const skillsList = document.getElementById(listId).getElementsByTagName("li");
-    return Array.from(skillsList).map(skill => skill.innerText);
-}
-
-function hasEnoughSkills(requiredSkills, userSkills, count) {
-    const matchingSkills = userSkills.filter(skill => requiredSkills.includes(skill));
-    return matchingSkills.length >= count;
+    const skillsArray = Array.from(skillsList).map(skill => skill.innerText.replace("Remove", "").trim());
+    console.log(`Skills in ${listId}: ${skillsArray.join(", ")}`);
+    return skillsArray;
 }
 
 
